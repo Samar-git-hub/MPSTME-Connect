28389d3dc8f800790687f5a3562acbc57a3c769d
Samar-git-hub <samarkamat88@gmail.com>
2024-09-26 21:26:54 +0530

    milestone 3
    So for the implementation of the toggle for light and dark mode, I made a button that has an image that switches between a moon icon for dark mode and a sun icon for light mode. The script element is in the head, so I put DOMContentLoaded, so that the javascript logic will only begin after the entire document object model loads. So first part of the js code was basically making sure that by default the users page looks a certain way, as their browser must have set, and the second part (on click onwards) is for everytime the button is pressed. Specifically, when the button is pressed or toggled, we get the attribute that contained theme in the html tag document.documentElement) to check what it is, if it is dark we change it to light, and vice versa. Everytime we also reassign the local storage so that after refreshes, the users preferences stay intact.

---
+++ static/script.js
@@ -0,0 +1,33 @@
+// when all dom is loaded, this will execute, this is basically for toggling between dark and light mode
+document.addEventListener("DOMContentLoaded", function () {
+    const buttonelement = document.getElementById("toggle-button")
+    const defaultTheme = localStorage.getItem('theme') || 'dark'; // browsers have this, if not present, set default as dark
+
+    document.documentElement.setAttribute('data-theme', defaultTheme);
+    // since I havent defined any css related to data-theme for dark, it will use the dault ^
+    // document.documentElement is a way to access the <html> tag (root element)
+
+    if (defaultTheme === 'light') {
+        buttonelement.src = '/static/suntheme.png';
+    } else {
+        buttonelement.src = '/static/moontheme.png';
+    }
+    // upper part is basically like a get request, this is what the user sees whenever he comes to the page, even if its a reload
+    // below part is the actual logic for changing the colors
+    buttonelement.addEventListener('click', function () {
+        const currentTheme = document.documentElement.getAttribute('data-theme'); // for this logic, we first get the current theme 
+        // which is in the html element (document.documentElement)
+        if (currentTheme === 'dark')
+        {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+            buttonelement.src = '/static/suntheme.png';
+        }
+        else
+        {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+            buttonelement.src = '/static/moontheme.png';
+        }
+    })
+})
